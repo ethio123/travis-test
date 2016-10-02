96ea14db6a4581149bf57f4ed88cf65cea87a1f8
dele <jiajiwu.icare@gmail.com>
2016-10-02 11:50:04 +0200

    t2

--- scala/app/controllers/Application.scala
+++ scala/app/controllers/Application.scala
@@ -38,6 +38,15 @@ class Application @Inject()(
                            )
                            extends Controller with I18nSupport {
 
+  def index = Action {
+    /** change the template here to use a different way of compilation and loading of the ts ng2 app.
+      * index()  :    does no ts compilation in advance. the ts files are download by the browser and compiled there to js.
+      * index1() :    compiles the ts files to individual js files. Systemjs loads the individual files.
+      * index2() :    add the option -DtsCompileMode=stage to your sbt task . F.i. 'sbt ~run -DtsCompileMode=stage' this will produce the app as one single js file.
+      */
+    Ok(views.html.index1())
+  }
+
   //val helloActor = actorSys.actorOf(TestActor.props, "hello-actor")
 
   val dbConfig = dbConfigProvider.get[JdbcProfile]
