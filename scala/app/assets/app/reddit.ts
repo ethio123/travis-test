c7146a46571e6aafdd3a8a0847af844a1fdf3c79
dele <jiajiwu.icare@gmail.com>
2016-10-04 09:16:15 +0200

    css

---
+++ scala/app/assets/app/reddit.ts
@@ -0,0 +1,33 @@
+import {Component} from "@angular/core"
+import {bootstrap} from "@angular/platform-browser-dynamic"
+
+@Component({
+  selector: "todo-reddit",
+  template: `
+<form class="ui large form segment">
+  <h3 class="ui header">Add a Link</h3>
+
+  <div class="field">
+    <label for="title">Title:</label>
+    <input name="title" #newTitle>
+  </div>
+
+  <div class="field">
+    <label for="link">Link:</label>
+    <input name="link" #newLink>
+  </div>
+
+  <button (click)="addArticle(newTitle, newLink)">Submit</button>
+</form>
+`
+})
+
+class RedditComponent {
+
+
+  addArticle(title:HTMLInputElement, link:HTMLInputElement):void {
+    console.log(`Adding article, title: ${title.value} and link: ${link.value}`)
+  }
+}
+
+bootstrap(RedditComponent)
